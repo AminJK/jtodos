3d9d8a17f89bc6b49c5e6b27a107aec56d04d53a
niufennan <626211114@qq.com>
2017-12-19 22:53:28 +0800

    v1-11_2

--- src/main/java/com/niufennan/jtodos/persistence/impl/JpaTodoRepository.java
+++ src/main/java/com/niufennan/jtodos/persistence/impl/JpaTodoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.criteria.*;
 import java.util.List;
@@ -15,23 +16,21 @@ import java.util.List;
 @Repository
 public class JpaTodoRepository implements TodoRepository {
 
-    @PersistenceUnit
-    private EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    private EntityManager entityManager;
     public List<Todo> getAll() {
-        CriteriaQuery<Todo> criteriaQuery=entityManagerFactory.createEntityManager().getCriteriaBuilder().createQuery(Todo.class);
-        return entityManagerFactory.createEntityManager().createQuery(criteriaQuery).getResultList();
+        CriteriaQuery<Todo> criteriaQuery=entityManager.getCriteriaBuilder().createQuery(Todo.class);
+        return entityManager.createQuery(criteriaQuery).getResultList();
     }
-
     public List<Todo> getTodoByUserId(int userId) {
-        CriteriaBuilder builder=entityManagerFactory.createEntityManager().getCriteriaBuilder();
+        CriteriaBuilder builder=entityManager.getCriteriaBuilder();
         CriteriaQuery<Todo> criteriaQuery=builder.createQuery(Todo.class);
         Root<Todo> todoRoot = criteriaQuery.from(Todo.class);
         Predicate predicate = builder.equal(todoRoot.get("userId"), userId);
         criteriaQuery.where(predicate);
-        return entityManagerFactory.createEntityManager().createQuery(criteriaQuery).getResultList();
+        return entityManager.createQuery(criteriaQuery).getResultList();
     }
-
     public void save(Todo todo) {
-        entityManagerFactory.createEntityManager().persist(todo);
+        entityManager.persist(todo);
     }
 }
